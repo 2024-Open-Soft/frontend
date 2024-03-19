7e17eac2ccca5fe2104ea79d1cdae8ff97cc1855
harshkhasbage77 <harshkhasbage77@gmail.com>
2024-03-19 22:46:38 +0530

    Feat: Legacy Redux store removed; Redux-toolkit utilized;

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,24 @@
-import { combineReducers, applyMiddleware, configureStore } from 'redux';
-import { legacy_createStore as createStore} from 'redux'
-import {thunk} from 'redux-thunk'; // Optional: for async actions
+// import { combineReducers, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
+// import { legacy_createStore as createStore} from 'redux'
+// import {thunk} from 'redux-thunk'; // Optional: for async actions
 
 // Import your reducers here
-import counterReducer from '../reducers/counter';
+// import counterReducer from '../reducers/counter';
 
 // Combine reducers if you have multiple
-const rootReducer = combineReducers({
-  counter: counterReducer,
-});
+// const rootReducer = combineReducers({
+//   counter: counterReducer,
+// });
 
 // Create the Redux store with rootReducer and middleware
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+})
 
 export default store;
