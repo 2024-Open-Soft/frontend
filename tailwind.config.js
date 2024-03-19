b1b9f458421fb69d30bfdcd4c82e6530aca03386
harshkhasbage77 <harshkhasbage77@gmail.com>
2024-03-20 03:12:55 +0530

    Feat: Tailwindcss and MUI set up done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  important: '#root',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
