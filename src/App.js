b7a6c9347fafc01aba4dc3aeab71c1e62827d686
Ansh Sahu <anshsahu1402@gmail.com>
2024-03-22 02:35:56 +0530

    Background-Player

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './index.css';
 import { Counter } from './features/counter/counter';
-
+import BackgroundPlayer from './components/BackgroundPlayer';
 import Button from '@mui/material/Button';
 
 function App() {
   return (
     <>
     <div className="App">
-      <header className="App-header text-center ">
+      {/* <header className="App-header text-center ">
       <h1 className='text-2xlg font-sans'>Redux Store has been set up</h1>
         <Counter />
         <p className='text-lg font-mono'>tailwindcss set up done</p>
@@ -15,7 +15,8 @@ function App() {
         <br />
         <Button variant="contained">Material UI set up done</Button>
       </div>
-      </header>
+      </header> */}
+      <BackgroundPlayer />
     </div>
     </>
   );
