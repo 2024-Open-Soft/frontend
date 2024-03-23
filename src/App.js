b975c6814269748ad6f4156f96da58d1c2909ef6
Abhay Sharma <abhaystar2004@kgpian.iitkgp.ac.in>
2024-03-23 16:28:59 +0530

    responsive for mobile ��

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './index.css';
 // import { Counter } from './features/counter/counter';
-import LoginPage from './components/login/login.js';
+import SignUp from './components/login/login.js';
 // import Button from '@mui/material/Button';
 
 function App() {
   return (
     <main>
-      <LoginPage />
+      {/* <LoginPage /> */}
+      <SignUp />
     </main>
   );
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,25 +1,88 @@
+// import { React } from "react";
 import React, { useState } from "react";
-import Button from "@mui/material/Button";
-import { MuiOtpInput } from "mui-one-time-password-input";
+import {
+  Box,
+  Button,
+  CssBaseline,
+  Grid,
+  Link,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Autocomplete from "@mui/material/Autocomplete";
-import TextField from "@mui/material/TextField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { MuiOtpInput } from "mui-one-time-password-input";
 
-const LoginPage = () => {
-  const [countryCode, setCountryCode] = useState("+91");
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#000000b3",
+      paper: "#000000b3",
+    },
+    text: {
+      primary: "#ffffffb3",
+      secondary: "#ccccccb3",
+    },
+  },
+  typography: {
+    fontFamily: "Arial",
+  },
+  components: {
+    MuiGrid: {
+      styleOverrides: {
+        root: {
+          backgroundImage: "unset",
+          padding: "0 2rem",
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          borderRadius: "15px",
+          "& fieldset": {
+            borderColor: "#ffffff83",
+          },
+        },
+        input: {
+          backgroundColor: "#FFFFFF00",
+          color: "#ffffffb3",
+          "&:-webkit-autofill": {
+            WebkitBoxShadow: "0 0 0 200px #0000009e inset",
+            WebkitTextFillColor: "#ffffffb3",
+            caretColor: "#ffffff",
+          },
+        },
+      },
+    },
+  },
+});
 
-  const countryCodes = [
+const countryCodes = [
     { code: "+91", country: "India" },
     { code: "+1", country: "USA" },
     { code: "+44", country: "UK" },
     { code: "+61", country: "Australia" },
     { code: "+81", country: "Japan" },
-  ];
+];
 
-  const handleChange = (event) => {
-    setCountryCode(event.target.value);
+
+const LoginPage = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      countryCode: data.get("countryCode"),
+      phone: data.get("phone"),
+      otp: value,
+    });
   };
-
+  
   const [value, setValue] = React.useState("");
+
+  const [countryCode, setCountryCode] = React.useState("+91");
 
   const handleValueChange = (newValue) => {
     setValue(newValue);
@@ -27,71 +90,180 @@ const LoginPage = () => {
 
   return (
     <section
-      className="flex font-sans items-center justify-center h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('/bg.jpg')" }}
+      className="flex flex-col items-center justify-center"
+      style={{
+        backgroundImage: "url('/bg.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+      }}
     >
-      <div className="bg-black bg-opacity-40 w-1/2 h-3/5 rounded-3xl filter backdrop-filter backdrop-blur-md flex flex-row overflow-hidden">
-        <div
-          className="flex items-start bg-cover h-full w-2/5"
-          style={{ backgroundImage: "url('/bg1.jpg')" }}
-        ></div>
-        <div className="w-3/5 p-4 h-full">
-          <h1 className="text-2xl ml-10 mt-10 font-semibold text-white">
-            Enter Phone Number
-          </h1>
-          <form className="flex flex-row items-center justify-center mt-8">
-            <Autocomplete
-              id="combo-box-demo"
-              options={countryCodes}
-              getOptionLabel={(option) => option.code}
-              renderInput={(params) => (
-                <TextField {...params} variant="outlined" />
-              )}
-              onChange={(event, newValue) => {
-                setCountryCode(newValue);
-              }}
-              className="mr-4 w-auto h-11 rounded-lg border-white bg-transparent opacity-80 border-[1px]"
-              style={{ color: "white", fontSize: "20px" }}
-            />
-            <input
-              className="mr-8 w-1/2 h-11 rounded-lg border-white bg-transparent opacity-80 border-[1px]"
-              placeholder="000 000 0000"
-              style={{ color: "white", fontSize: "20px", padding: "1rem" }}
-            />
-            <Button
+      <div>
+        <ThemeProvider theme={theme}>
+          <Grid
+            container
+            component="main"
+            sx={{
+              px: {
+                xs: "5vw",
+                md: "15vw",
+              },
+              py: {
+                xs: "1vh",
+                md: "5vh",
+              },
+            //   width: "90%",
+            }}
+          >
+            <CssBaseline />
+            <Grid
+              item
+              xs={false}
+              sm={false}
+              md={5}
               sx={{
-                borderRadius: "10rem",
-                opacity: "90%",
-                width: "6rem",
-                height: "3rem",
-                boxShadow: "0",
+                backgroundImage: "url('/bg1.jpg')",
+                backgroundRepeat: "no-repeat",
+                backgroundColor: (t) =>
+                  t.palette.mode === "light"
+                    ? t.palette.grey[50]
+                    : t.palette.grey[900],
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                borderRadius: "50px 0 0 50px",
               }}
-              size="large"
-              variant="contained"
+            />
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={7}
+              component={Paper}
+              elevation={6}
+              square
+              sx={{
+                borderRadius: {
+                  xs: "25px",
+                  md: "0 50px 50px 0",
+                },
+                backdropFilter: "blur(15px)",
+              }}
             >
-              Send
-            </Button>
-          </form>
-          <h1 className="text-2xl ml-10 mt-10 font-semibold text-white">
-            Enter OTP
-          </h1>
-          <form className="flex flex-row items-center justify-center mt-8">
-            <MuiOtpInput
-                className="mr-8"
-                sx={{borderRadius: "10rem", opacity: "90%", width: "70%", height: "3rem", boxShadow: "0"
-                  }}
-                  length={6}
-                  value={value}
-                  onChange={handleValueChange}
-                
-            />
-            <Button
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  my: {
+                    xs: 4,
+                    sm: 6,
+                    md: 8,
+                  },
+                  mx: {
+                    xs: 0,
+                    sm: 2,
+                    md: 3,
+                  },
+                }}
+              >
+                <Box
+                  component="form"
+                  noValidate
+                  display={"flex"}
+                  flexDirection={"column"}
+                  onSubmit={handleSubmit}
+                  sx={{ mt: 1 }}
+                >
+                        <Typography variant="" sx={{ fontSize: "large" }}>
+                          Enter Phone Number
+                        </Typography>
+                    <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: {xs: "column", md: "row"},
+                        alignItems: "center",
+                        width: "100%",
+
+                    }}
+                    >
+                    <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        width: "100%",
+                        md: {flexDirection: "column"}
+                    }}
+                    >
+                  <Autocomplete
+                    id="combo-box-demo"
+                    options={countryCodes}
+                    getOptionLabel={(option) => option.code}
+                    renderInput={(params) => <TextField {...params} onSubmit={handleSubmit} id="countryCode" name="countryCode" variant="outlined" />}
+                    sx={{ mt: 1, mb: 2, mr: 2, width: "5rem"}}
+                    onChange={(event, newValue) => {
+                        setCountryCode(newValue);
+                    }}
+                    />
+                    <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="phone"
+                    name="phone"
+                    autoComplete="phone"
+                    placeholder="000 000 0000"
+                    autoFocus
+                    sx={{ mt: 1, mb: 2, mr: 2}}
+                  />
+                  </Box>
+                  <Button
+                    sx={{
+                        borderRadius: "10rem",
+                        opacity: "90%",
+                        width: {xs: "100%", md: "15rem"},
+                        height: "3.5rem",
+                        backgroundColor: "#3076A8",
+                        boxShadow: "0",
+                        mb: 2
+                    }}
+                    size="large"
+                    variant="contained"
+                    >
+                    Send
+                  </Button>
+
+                  </Box>
+                  <Typography variant="" sx={{ fontSize: "large",mb: 1 }}>
+                    Enter OTP
+                  </Typography>
+                    <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: {xs: "column", md: "row"},
+                        alignItems: "center",
+                        width: "100%",
+                    }}
+                    >
+                  <MuiOtpInput
+                    length={6}
+                    value={value}
+                    onChange={handleValueChange}
+                    onSubmit={handleSubmit}
+                    id="otp"
+                    name="otp"
+                    sx={{gap: "10px", borderRadius: "10rem", height: "auto", mr: 2,}}
+                    className="otp"
+                    />
+                    <Button
               sx={{
                 borderRadius: "10rem",
+                width: {xs: "100%", md: "15rem"},
+                height: "3.5rem",
                 opacity: "90%",
-                width: "6rem",
-                height: "3rem",
                 boxShadow: "0",
+                mt: 2,
+                pt: 1,
               }}
               size="large"
               color="success"
@@ -99,14 +271,35 @@ const LoginPage = () => {
             >
               Verify
             </Button>
-          </form>
-          <div className="mt-[12rem] ml-10 italic opacity-80 text-xl text-white">
-            Having trouble logging in?{" "}
-            <a href="#" className="decoration-[none]">
-              Get Help
-            </a>
-          </div>
-        </div>
+                    </Box>
+                  <Button
+                    type="submit"
+                    // fullWidth
+                    variant="contained"
+                    sx={{
+                        borderRadius: "10rem",
+                        width: {xs: "100%", md: "15rem"},
+                        height: "3.5rem",
+                        opacity: "90%",
+                        boxShadow: "0",
+                        backgroundColor: "#BB4D4D",
+                        mt: 2,
+                      }}
+                      size="large"
+                  >
+                    Next Step
+                  </Button>
+                  <Typography align="left" sx={{ mt: 2, fontStyle: 'italic', opacity: '80%' }}>
+                    {"Having trouble logging in? "}
+                    <Link href="#" variant="body1">
+                      {"Get Help"}
+                    </Link>
+                  </Typography>
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
+        </ThemeProvider>
       </div>
     </section>
   );
