fb610e3010ed3b3afc5a926b475f0c41295d8049
Shubham Chadokar <shubhamchadokar36@gmail.com>
2024-03-23 21:50:27 +0530

    home changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,40 @@
 import React from "react";
 import BackgroundPlayer from "../components/BackgroundPlayer";
 import { SideBar } from "../components/SideBar";
+import Cars from "../assets/Cars.mp4";
+import { Outlet } from "react-router-dom";
 
 const Home = ({ isBgVideo }) => {
   return (
-    <div className="">
-      <BackgroundPlayer />
-      <SideBar />
+    <div className="bg-black">
+      <div className={`w-full relative z-[1] ${isBgVideo && "bg-black"}`}>
+        <a href="/" className="flex fixed mt-3 left-[1%]">
+          <img src="/Logo.svg" alt="WHO" className="w-36 " />
+        </a>
+        <SideBar />
+        <Outlet />
+      </div>
+
+      <div className="absolute top-0 right-0 -z-0 w-full">
+        <div className="w-full">
+          <video
+            src={Cars}
+            autoPlay
+            muted
+            loop
+            className="w-full object-cover h-screen"
+          />
+        </div>
+        <div className="sm:bg-gradient-to-t sm:from-black sm:via-transparent to-red-00 absolute top-0 text-3xl flex flex-row justify-center items-center w-full h-screen text-white"></div>
+        <div class="absolute left-0 top-0 h-screen w-full sm:bg-gradient-to-l sm:from-transparent sm:to-black"></div>
+        {/* <div
+          className="absolute left-0 top-0 h-screen w-full"
+          style={{
+            background:
+              "linear-gradient(to left, rgba(255, 0, 0, 0), rgba(0, 0, 0, 1))",
+          }}
+        ></div> */}
+      </div>
     </div>
   );
 };
