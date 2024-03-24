845d8fa9208b61d7d5f36143090602a9ac7188ec
Vaibhav Mohite <vaibhavdmohite22@gmail.com>
2024-03-24 17:36:08 +0530

    add: user info page

--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -1,7 +1,39 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import Account from "../components/Account";
+import Plan from "../components/Plan";
 
 const UserInfo = () => {
-  return <div>UserInfo</div>;
+
+  const gridStyle = {
+    minWidth: "300px",
+    maxWidth: "1000px",
+    width: "90%",
+    mt: {
+      lg: "100px"
+    },
+    borderRadius: "20px",
+    background: "linear-gradient(60deg, #1d1d1d, #ffffff1f)",
+    backdropFilter: "blur(15px)",
+    margin: "auto",
+    p: 4
+  }
+
+  return (
+    <>
+      <Grid container sx={gridStyle} spacing={2}>
+        <Grid item xs={12}>
+          <Account />
+        </Grid>
+        <Grid item xs={12}>
+          <Plan />
+        </Grid>
+        <Grid item xs={12}>
+
+        </Grid>
+      </Grid>
+
+    </>)
 };
 
 export default UserInfo;
