b5d7e18d2a93f83da895c1c50a9d3b9d4308c120
harshkhasbage77 <harshkhasbage77@gmail.com>
2024-03-26 02:20:07 +0530

    Component: Subscription form done

---
+++ src/components/MonthsSelect.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography, Box, Autocomplete, Grid } from "@mui/material";
+
+const MonthsSelect = () => {
+    return (
+        <Grid
+         >
+
+        </Grid>
+    );
+}
+
+export default MonthsSelect;
